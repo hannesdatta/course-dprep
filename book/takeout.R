f3001db53e45579cce9983265fae5e5362d45e94
Hannes Datta <h.datta@tilburguniversity.edu>
2025-09-02 22:40:44 +0200

    also update terminal

---
+++ book/takeout.R
@@ -0,0 +1,14 @@
+
+Here’s a simplified diagram of this cycle:
+  
+  ```{mermaid}
+flowchart LR
+A[Product Backlog] --> B[Sprint Planning]
+B --> C[Sprint Backlog]
+C --> D[Development Work]
+D --> E[Daily Stand-Up]
+D --> F[Sprint Review]
+F --> G[Increment]
+G --> H[Retrospective]
+H --> A
+```
